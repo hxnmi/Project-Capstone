26df53855cca73b9f11fdb046064732c4f138fc4
Prakasa2021 <uzumakidef@gmail.com>
2022-10-03 22:10:32 +0700

    Add Materials and Prefabs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,65 +10,73 @@ public class PlayerController : MonoBehaviour
 
     [Header("Lane Movement")]
     [SerializeField] private int desiredLane = 1;
-    [SerializeField] private float laneDistance;
+    [SerializeField] private float laneDistance = 4;
 
     [Header("Jump")]
     [SerializeField] private float jumpForce;
     [SerializeField] private float Gravity;
 
-    // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         direction.z = speed;
 
-        if(controller.isGrounded){
+        if(controller.isGrounded)
+        {
             direction.y = -1;
-            if(Input.GetKeyDown(KeyCode.UpArrow)){
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+            {
                 Jump();
             }
-        }else{
+        }
+        else{
             direction.y += Gravity * Time.deltaTime;
         }
         
 
-        if(Input.GetKeyDown(KeyCode.RightArrow)){
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
             desiredLane++;
-            if(desiredLane == 3){
+            if(desiredLane == 3)
+            {
                 desiredLane = 2;
             }
-        }else if(Input.GetKeyDown(KeyCode.LeftArrow)){
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow)){
             desiredLane--;
-            if(desiredLane == -1){
+            if(desiredLane == -1)
+            {
                 desiredLane = 0;
             }
         }
 
         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
 
-        if(desiredLane == 0){
+        if(desiredLane == 0)
+        {
             targetPosition += Vector3.left * laneDistance;
-        }else if(desiredLane == 2){
+        }
+        else if(desiredLane == 2)
+        {
             targetPosition += Vector3.right * laneDistance;
         }
 
         transform.position = Vector3.Lerp(transform.position, targetPosition, 80 * Time.deltaTime);
 
-        // transform.position = targetPosition;
-
-        Debug.Log(transform.position);
+        transform.position = targetPosition;
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate() 
+    {
         controller.Move(direction * Time.fixedDeltaTime);
     }
 
-    private void Jump(){
+    private void Jump()
+    {
         direction.y = jumpForce;
     }
 }
