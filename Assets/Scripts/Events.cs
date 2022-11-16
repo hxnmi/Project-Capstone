939d0c5aa4490caa10f08609645fc0574cb4822b
M T Y P <uzumakidef@gmail.com>
2022-11-16 21:34:22 +0700

    UI Mechanism

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -8,6 +8,12 @@ public class Events : MonoBehaviour
     public GameObject panelKuis;
     public GameObject gameOverpanel;
     public KuisManager kuisManager;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
     public void ReplayGame()
     {
         SceneManager.LoadScene("Level");
@@ -16,6 +22,21 @@ public class Events : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void Shop()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void About()
+    {
+        SceneManager.LoadScene("About");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 
     public void KuisRevive()
